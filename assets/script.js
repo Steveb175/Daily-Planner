bd4963a009b1614371bfc29352e2f639f678e065
Stephen Bondurant <stephenbondurant2014@gmail.com>
2023-03-11 18:01:04 -0600

    Local storage done.

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,42 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+
+// Current Date and Time
+$("#currentDay").text(dayjs().format("dddd, MMM D, hh:mm a"));
+setInterval(function currentTime() {
+  $("#currentDay").text(dayjs().format("dddd, MMM D, hh:mm a"));
+}, 1000);
+
+// Current time
+
+var currentTime = dayjs();
+
+// Sets current time to hour
+
+currentTime = currentTime.startOf("hour");
+$("#test-time").text(currentTime);
+
+// Current
+
+// Time set at the start of the day (+9 hours)
+var beginningTime = dayjs().startOf("day").add(9, "hours");
+
+//Event Listener to save input to local storage --this works!
+$(".saveBtn").click(function () {
+  var inputValue = $(this).siblings(".input").val();
+  var listHour = $(this).parent().data("hour");
+  localStorage.setItem(listHour, inputValue);
+});
+
+//for loop to loop through and get from local storage --IT WORKS!
+var businessHoursArray = [9, 10, 11, 12, 1, 2, 3, 4, 5];
+for (var i = 0; i < businessHoursArray.length; i++) {
+  var dataSetHour = localStorage.getItem(businessHoursArray[i]);
+  $("#input" + businessHoursArray[i]).val(dataSetHour);
+}
+
+$(document).ready(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -21,3 +56,12 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+
+// function pastPresentFuture() {
+//   businessHour9 = dayjs().startOf("day").add(9, "hours");
+//   currentTime = currentTime.startOf("hour");
+//   if (currentTime.isAfter(businessHour9)) {
+//     $("#input9").addClass("past");
+//   } else if (current {
+//   }
+// }
